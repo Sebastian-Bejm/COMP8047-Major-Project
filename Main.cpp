42b7e36435660dfb6430afccc7ad85f839bbb645
Sebastian Bejm <125sbejm@gmail.com>
2022-01-05 16:38:33 -0800

    Setup OpenGL basic rendering

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,57 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 800
+
+int initialize() {
+	return 0;
+}
+
+void teardown() {
+	
+}
+
 int main() {
+
+	// Init GLFW
+	glfwInit();
+
+	// Tell GLFW what version we are using (OpenGL 3.3)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create GLFW window
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "COMP 8047 Major Project", NULL, NULL);
+	// If window fails to create it exits
+	if (window == NULL) {
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Load GLAD so it configures OpenGL
+	gladLoadGL();
+
+	// Specify the viewport of OpenGL in the window
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	// Specify the color of the background (silver)
+	glClearColor(192.0f / 255.0f, 192.0f / 255.0f, 192.0f / 255.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	// Main loop
+	while (!glfwWindowShouldClose(window)) {
+		// todo create Update functions for rendering, physics, manager, etc.
+		glfwPollEvents();
+	}
+
+	// Destroy window when done and exit
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
