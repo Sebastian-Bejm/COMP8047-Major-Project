ace03337c76e73e5bf8d7b27dc420bfca902dcd1
Sebastian Bejm <125sbejm@gmail.com>
2022-01-10 14:54:58 -0800

    comments for gameobject

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,19 +1,26 @@
 #include "GameObject.h"
 
+// Constructor to create a new GameObject
+// A GameObject requires a tag, shape, shader, and transform
 GameObject::GameObject(std::string tag, ShapeType shapeType, Shader& shader, glm::vec3 position, glm::vec3 rotation, glm::vec3 scale) {
 
 	objectTag = tag;
 	shaderProgram = shader;
 
+	// Retrieve vertices/indices data for a shape
 	ShapeDetails shapeDetails;
 	Shape shape = shapeDetails.GetShape(shapeType);
 	
 	std::vector<Vertex> vertices = shape.vertices;
 	std::vector<GLuint> indices = shape.indices;
 
+	// Create a mesh with the vertices, indices, and transform
 	mesh = Mesh(vertices, indices);
 	transform = Transform(position, rotation, scale);
 
+	// Texturing stuff will go here...
+
+	// Set the model matrix 
 	GLint cubeLoc = glGetUniformLocation(shaderProgram.GetID(), "model");
 	glUniformMatrix4fv(cubeLoc, 1, GL_FALSE, glm::value_ptr(transform.GetModelMatrix()));
 }
@@ -22,6 +29,8 @@ Transform& GameObject::GetTransform() {
 	return transform;
 }
 
+// Update this GameObject's matrices. 
+// Uses the camera's view and projection matrices to update everything accordingly
 void GameObject::Update(Camera& camera) {
 	// Update the object model matrix first
 	GLint cubeLoc = glGetUniformLocation(shaderProgram.GetID(), "model");
@@ -31,6 +40,7 @@ void GameObject::Update(Camera& camera) {
 	mesh.Draw(shaderProgram, camera);
 }
 
+// Delete the contents of this GameObject
 void GameObject::Delete() {
 	mesh.Delete();
 	shaderProgram.Delete();
