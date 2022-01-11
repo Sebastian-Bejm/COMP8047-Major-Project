0797b379bdfe2fd3051300c51e56c4939f3fa19f
Sebastian Bejm <125sbejm@gmail.com>
2022-01-11 12:19:55 -0800

    Cleaned up old code

--- Main.cpp
+++ Main.cpp
@@ -12,34 +12,6 @@ const int screenHeight = 800;
 float deltaTime;
 float currentFrame, lastFrame;
 
-// Pyramid
-Vertex pyramidVerts[] =
-{
-	glm::vec3(-0.5f, 0.0f, 0.5f),	glm::vec3(0.8f, 0.7f, 0.4f),	glm::vec2(0.0f, 0.0f), // Lower left corner
-	glm::vec3(-0.5f, 0.0f, -0.5f),	glm::vec3(0.8f, 0.7f, 0.4f),	glm::vec2(5.0f, 0.0f), // Upper left corner
-	glm::vec3(0.5f, 0.0f, -0.5f),	glm::vec3(0.8f, 0.7f, 0.4f),	glm::vec2(0.0f, 0.0f), // Upper right corner
-	glm::vec3(0.5f, 0.0f, 0.5f),	glm::vec3(0.8f, 0.7f, 0.4f),	glm::vec2(5.0f, 0.0f),// Lower right corner
-	glm::vec3(0.0f, 0.8f, 0.0f),	glm::vec3(0.95f, 0.85f, 0.7f),	glm::vec2(2.5f, 5.0f),// Lower left corner
-};
-
-GLfloat pyramidVertices[] =
-{ //     COORDINATES     /        COLORS      /   TexCoord  //
-	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
-	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
-	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
-	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
-	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
-};
-
-GLuint pyramidIndices[] = {
-	0, 1, 2,
-	0, 2, 3,
-	0, 1, 4,
-	1, 2, 4,
-	2, 3, 4,
-	3, 0, 4
-};
-
 // TODO: refactor later for initalize, update, and teardown
 int Initialize() {
 	return 0;
@@ -114,7 +86,7 @@ int main() {
 	ShapeDetails shapeDetails;
 	Shape pyramid = shapeDetails.GetShape(PYRAMID);
 
-	// already sized
+	// vectors are already sized correctly
 	std::vector<Vertex> verts = pyramid.vertices;
 	std::vector<GLuint> inds = pyramid.indices;
 
@@ -129,27 +101,6 @@ int main() {
 
 	//GameObject texPyramid("TexPyrm", "brick.png", PYRAMID, textureShader,
 	//	glm::vec3(0.0f, 0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
-
-	// Generates Vertex Array Object and binds it
-	/*VAO VAO1;
-	VAO1.Bind();
-
-	// Generates Vertex Buffer Object and links it to vertices
-	VBO VBO1(pyramidVertices, sizeof(pyramidVertices));
-	// Generates Element Buffer Object and links it to indices
-	EBO EBO1(pyramidIndices, sizeof(pyramidIndices));
-
-	// Links VBO attributes such as coordinates and colors to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	// Unbind all to prevent accidentally modifying them
-	VAO1.Unbind();
-	VBO1.Unbind();
-	EBO1.Unbind();
-
-	Texture texture("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	texture.TexUnit(textureShader, "tex0", 0);*/
 
 	// Specify the color of the background (silver)
 	glClearColor(192.0f / 255.0f, 192.0f / 255.0f, 192.0f / 255.0f, 1.0f);
@@ -180,12 +131,9 @@ int main() {
 		camera.SetMatrix(45.0f, 0.1f, 100.0f);
 
 		glm::mat4 model = glm::mat4(1.0f);
-		//glm::mat4 cam = camera.GetCameraMatrix();
 
 		GLint modelLoc = glGetUniformLocation(textureShader.GetID(), "model");
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		//GLint camLoc = glGetUniformLocation(textureShader.GetID(), "camMatrix");
-		//glUniformMatrix4fv(camLoc, 1, GL_FALSE, glm::value_ptr(cam));
 
 		mesh.Draw(textureShader, camera);
 
@@ -198,28 +146,6 @@ int main() {
 			//texPyramid.GetTransform().Translate(glm::vec3(0.5f, 0.0f, 0.0f), deltaTime);
 		}*/
 
-		//texPyramid.Update(camera);
-
-		//texSquare.Update(camera);
-
-		//cubeObject.Update(camera); // the leftmost
-		//middleCube.Update(camera); 
-		//firstCube.Update(camera); // the right
-
-		//GLint cubeLoc = glGetUniformLocation(cubeShader.GetID(), "model");
-		//glUniformMatrix4fv(cubeLoc, 1, GL_FALSE, glm::value_ptr(transform.GetModelMatrix()));
-
-		//pyramidMesh.Draw(shaderProgram, camera); 
-		//cubeMesh.Draw(cubeShader, camera);
-
-		//textureShader.Activate();
-
-		//texture.Bind();
-
-		//VAO1.Bind();
-
-		//glDrawElements(GL_TRIANGLES, sizeof(pyramidIndices)/sizeof(int), GL_UNSIGNED_INT, 0);
-
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 
@@ -228,23 +154,8 @@ int main() {
 	}
 
 	// Cleanup objects we have created
-	//shaderProgram.Delete();
-	//cubeShader.Delete();
-
-	//middleCube.Delete();
-	//firstCube.Delete();
-	//cubeObject.Delete();
-
-	//texPyramid.Delete();
-
 	mesh.Delete();
 	textureShader.Delete();
-
-	/*VAO1.Delete();
-	VBO1.Delete();
-	EBO1.Delete();
-	texture.Delete();
-	textureShader.Delete();*/
 
 	// Destroy window when done and exit
 	glfwDestroyWindow(window);
